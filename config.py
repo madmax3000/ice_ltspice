045ccf17158765dd27dc550d05a337e7108b426b
johnj <johnjose223@gmail.com>
2020-12-05 20:01:20 -0600

    added some instructions as due to the issue of casing while data entry

--- config.py
+++ config.py
@@ -8,10 +8,16 @@ LTSpice_executable_path = 'C:\Program Files\LTC\LTspiceXVII\XVIIx64.exe' # works
 LTSpice_asc_filename ='showfile.asc'
 
 # The measurements to be saved from the simulation. The appropriate numbers can be found in the .raw file generated at simulation.
-variable_numbering = {'time': 0, 'n002': 2, 'R1': 3}
+variable_numbering = {'time': 0, 'n002': 2, 'R3': 3}
+#{'the raw file variable name' : raw variable index, 'the raw file variable name' : raw vari index,}
+# raw file index starting from 1, 2 ,3 etc
+''' the time variable is :0 it is default as it is defined by the program,  the variable name 'noo2' is representing the variable names : the comming number which is indexed" 
+"represents the the indexing of the raw file variable starting from the counting number 1, 2, 3 etc" 
+"this reperesnts which data from raw file is to be saved into the csv file for further processing " '''
 # The ordering of the measurements to be saved in the output csv files can be changed below, by changing how the numbers are ordered.
 # E.g. switch place of 0 and 1 if you want V_c to be placed left of time in the output csv files.
 preffered_sorting = [0, 1, 2]
+# the plotting would have 0 for time , 1 for voltages or  2 for currents etc
 
 # Leave blank if output should be writtten to root folder. The folder specified must be created manually if it doesn't exist.
 output_data_path = "data"
@@ -19,3 +25,4 @@ output_data_path = "data"
 # Parameter names gives the files the name of the parameter that it is run with.
 # Assumes number by default. Set to 'parameter' or 'number'.
 output_data_naming_convention = 'number'
+#for encoding related data use AScii mode of compression for ltspice
